5d0a52dfc0abe636fa1d2f50810212b2c0824264
Matthew Pon <ixnoblesseobligexi@gmail.com>
2015-09-17 16:51:07 -0400

    Assignment 03 draft

---
+++ Assignment_03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movespeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){
+			transform.position += new Vector3(0f, 0f, 1f) * Time.deltaTime * movespeed;
+			transform.eulerAngles = new Vector3(0f, 0f, 0f);
+		}
+
+		Camera.main.transform.position = transform.position + new Vector3(0f, 0f, 1f);
+	}
+}

2e6b59fda0299b96118a639a98c0478f952ae858
Matthew Pon <ixnoblesseobligexi@gmail.com>
2015-09-17 16:33:02 -0400

    week 02

---
+++ week02/Assets/Scripts/PoopGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; // allows us to talk to Text Objects
+
+public class PoopGame : MonoBehaviour {
+
+	// Declare a variable to record points
+	float score = 0f; // declaring a decimal number that starts at zero
+
+	// We want this script to talk to our text UI object
+	// We need to tell unity which Text object to talk to
+	public Text mytext;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ask unity if player is holding down spacebar
+		if ( Input.GetKey ( KeyCode.Space) ){
+			// Add "time.deltaTIme" on top of existing score
+			// "Time.deltaTime" = frame duration
+			// also can be written as score = score + Time.deltaTime; 
+			score += Time.deltaTime;
+			//prints score to UI
+			mytext.text = ( score.ToString() );
+		}
+	}
+}
---
+++ week02/Assets/Scripts/PressQAlot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class PressQAlot : MonoBehaviour {
+
+	Text myText;//variable which text gameobject im talking to
+
+	int myScore = 0; // an integer is a whole number
+
+	// Use this for initialization
+	void Start () {
+		// Tells unity to try to find a text component on the same object
+		myText = GetComponent<Text>(); 
+		myText.text = "IT WOPRKS !! ITS ALIVE KSJD:FLK";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// detect if player pressed q (but will not fire again until player releases Q)
+		if (Input.GetKeyDown (KeyCode.Q)){
+			myScore += 1;
+			// or myScore = myScore + 1; or myScore++;
+		}
+		myText.text = (myScore.ToString());
+		if (myScore > 20){
+			myText.color = Color.red;
+		}
+	}
+}

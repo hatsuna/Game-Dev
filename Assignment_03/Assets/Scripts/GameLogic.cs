4c13312a9b95031a2471ed65c1dd0fa0c0a71838
Matthew Pon <ixnoblesseobligexi@gmail.com>
2015-09-22 04:03:19 -0400

    Assignment 03, Journal 03, 10Print notes

---
+++ Assignment_03/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	public Transform player;
+	public Text textUI;
+	public Text winUI;
+	float victorySize = 5f;
+	string winState = "";
+
+	// Update is called once per frame
+	void Update () {
+		string textBuffer = "";
+
+		if(winState != "" && Input.GetKeyDown (KeyCode.Space)) {  
+			Application.LoadLevel (0);  
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			Vector3 offset = transform.position - player.transform.position;
+			float distFromGoal = offset.magnitude;
+
+			if( distFromGoal <= victorySize){
+				winState = "You got booty!\nPress [Space] to restart";
+			}else{
+				winState = "You got crabs.\nThe Booty was " + distFromGoal + " units away.\nPress [Space] to restart";
+			}
+		}
+
+		textBuffer += "[W] Forward\n[S] Backwards\n[A] Left\n[D] Right\n[Space] GET BOOTY";
+
+		textUI.text = textBuffer;
+		winUI.text = winState;
+	}
+}
--- Assignment_03/Assets/Scripts/PlayerController.cs
+++ Assignment_03/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float movespeed = 5f;
+	public float turnspeed = 100f;
 
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.W)){
-			transform.position += new Vector3(0f, 0f, 1f) * Time.deltaTime * movespeed;
-			transform.eulerAngles = new Vector3(0f, 0f, 0f);
+			transform.Translate ( Vector3.forward * Time.deltaTime * movespeed/*, Camera.main.transform*/);
 		}
-
-		Camera.main.transform.position = transform.position + new Vector3(0f, 0f, 1f);
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate ( Vector3.back * Time.deltaTime * movespeed/*, Camera.main.transform*/);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0f, -(Time.deltaTime * turnspeed), 0f);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0f, Time.deltaTime * turnspeed, 0f);
+		}
+		//Camera.main.transform.position = transform.position + new Vector3(0f, 5f, -5f);
+		//Camera.main.transform.eulerAngles = transform.eulerAngles + new Vector3(5f, 0f, 0f);
 	}
 }

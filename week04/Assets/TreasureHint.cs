a5b66b8775037605bf5b826cb236298f065cac1a
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2015-09-23 01:16:12 -0400

    Week04 Tuesday notes

---
+++ week04/Assets/TreasureHint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TreasureHint : MonoBehaviour {
+
+	public Transform goalObject;
+	public Transform playerObject;
+	public Text uiText;
+
+	bool didIWinYet = false;
+
+	// Update is called once per frame
+	void Update () {
+		if((playerObject.position - goalObject.position).sqrMagnitude > 50f) {
+			uiText.text = "you're going realy far away now, this is bad.";
+		}
+
+		if(Vector3.Distance ( goalObject.position, playerObject.position ) > 20f) {
+			uiText.text = "you're getting closer!!!";
+		}
+
+		if(Vector3.Distance ( goalObject.position, playerObject.position) < 5f){
+			uiText.text = "press space to win";
+			if(Input.GetKeyDown (KeyCode.Space)){
+				didIWinYet = true;
+			}
+		}
+
+		if (didIWinYet){
+			uiText.text = "you won";
+		}
+	}
+}

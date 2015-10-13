9068f9c051c0d7e53fd87785c4546e432ae91429
Matthew Pon <ixnoblesseobligexi@gmail.com>
2015-10-13 17:27:01 +0900

    I may have just broke things

--- week05-07/Assets/Scripts/GameLogic.cs
+++ week05-07/Assets/Scripts/GameLogic.cs
@@ -6,32 +6,41 @@ public class GameLogic : MonoBehaviour {
 
 	public Transform RealCam;
 	public Transform DreamCam;
-//	public Text controlsUI;
+	public Light cameraLight;
 	public Text AwakeUI;
 	public Text hintUI;
-	public Light cameraLight;
+	public Text inventoryUI;
+	public Text controlsUI;
 	
-	bool isDreaming = false;
+	bool isDreaming = true;
 	float maxAwakeTime = 10.0f; //how long you can stay awake for
 	float minAwakeTime = 3.0f;
 	float awakeTimer = 10.0f; // how long you can currently stay awake for
 	// float awakeRestore = 0.01f; // how much time restores while dreaming, only needed if one is skewed
+	string GameState = "Start";
+	/*
+	 * Start - Start screen
+	 * Alive - playing
+	 * Dead - Death Screen
+	 * 
+	 */
+	string DeathTextBuffer;
+	bool canInteract = false; // determines if player can use nearItem
+	bool hasItem = false;
+	GameObject nearItem;
 
 	void Start (){
-		DreamCam.gameObject.SetActive (false);
-		RealCam.gameObject.SetActive (true);	
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void CheckWakeState(){
 		/*controlsUI.text += "\nQ to switch";
 		if( Input.GetKeyDown (KeyCode.Q)){
 			isDreaming = !isDreaming; // If dreaming -> not dreaming, if not dreaming -> now dreaming
 		}*/
-
+		
 		//Changing light intensity based on awake time
 		cameraLight.intensity = 10 * (awakeTimer / maxAwakeTime);	
-
+		
 		//WHAT IF YOU"RE NOT IN CONTROL OF YOUR DREAMING AND WAKEFULNESS
 		if (awakeTimer <= 0){
 			isDreaming = true;
@@ -43,17 +52,21 @@ public class GameLogic : MonoBehaviour {
 		} /*else if (awakeTimer >= ( maxAwakeTime)){ // Allows player to choose when to come out of dream state
 			isDreaming = false;
 		}*/ 
-
+		
 		AwakeUI.text = "" + awakeTimer;
-
-
+		
+		
 		// Give player feedback that they are falling asleep
 		if (awakeTimer <= (maxAwakeTime / 5.0f) && !isDreaming && awakeTimer >= (maxAwakeTime / 10.0f)){
-			AwakeUI.text = "Zzz";
+			hintUI.text = "\n\nSo tired..";
+		}
+		if (awakeTimer <= 0){
+			hintUI.text = "";
 		}
 		if(isDreaming ){//|| awakeTimer <= 0){
 			RealCam.gameObject.SetActive (false);
 			DreamCam.gameObject.SetActive (true);
+
 			/*if (awakeTimer <= 0){
 				isDreaming = true;
 			}*/
@@ -76,7 +89,94 @@ public class GameLogic : MonoBehaviour {
 				awakeTimer = 0;
 			}*/
 		}
-		// Use tags to differentiate between collided objects,
-		//		hazards, things to pick up, etc
+	}
+
+	// Update is called once per frame
+	void Update () {
+	switch(GameState){
+		case "Start":
+			hintUI.text = 
+				"Parasomnia\n" +
+					"a game by Matt\n\n" +
+					"Is that... an alarm?\n" +
+					"Press [Space] to wake up\n" +
+					"Don't let your dreams be dreams.";
+			
+			//probably insert some alarm sound thing here or have it periodically go off
+			if(Input.GetKeyDown (KeyCode.Space)){
+				hintUI.text = "";
+				GameState = "Alive";
+				//Activate both players *********************
+			}
+			break;
+		case "Alive":
+			controlsUI.text = "[WASD] to move\nMouse to look\n[Space] to wake up\n[R] to restart";
+			CheckWakeState();
+			if(Input.GetKeyDown (KeyCode.E)){
+				if (canInteract && !hasItem){
+					inventoryUI.text += "\n" + nearItem.name;
+					hintUI.text = "Picked up " + nearItem.name;
+					hasItem = true;
+				}
+			}
+			break;
+		case "Dead":
+			hintUI.text = DeathTextBuffer;
+			break;
+		default:
+			break;
+		}
+
+		//restart level
+		if(Input.GetKeyDown(KeyCode.R)){
+			Application.LoadLevel(0);
+		}
+	}
+
+
+	// Use tags to differentiate between collided objects,
+	//		hazards, things to pick up, etc
+	void OnTriggerEnter (Collider activator) { // this parameter will get automatically get filled in with the thing that enters it
+		hintUI.text = "Is a trigger? " + activator.isTrigger;
+
+		if (activator.gameObject.tag == "Player"){
+			hintUI.text = "GG I just broke my game";
+		}
+
+		if (activator.gameObject.tag == "Item"){
+			hintUI.text = "There's something here.\n[E] to pick up";	
+			nearItem = activator.gameObject;
+			canInteract = true;
+		} 
+		if (activator.gameObject.tag == "Hazard"){
+			hintUI.text = "you are dying right now";
+		}
+		if (activator.gameObject.tag == "Window Warning"){
+			hintUI.text = "You feel a gentle breeze.\nThere must be a window nearby.";
+		}
+		if (activator.gameObject.tag == "Window"){
+			DeathTextBuffer = "You stumble into the window and crash though the glass.\nYou died.\n\nPress [R] to restart.";
+			GameState = "Dead";
+		}
+		
+	}
+
+	void OnTriggerExit (Collider deactivated) {
+		if (deactivated.gameObject.tag == "Item"){
+			canInteract = false;
+			if (hasItem){
+				deactivated.gameObject.SetActive (false);
+			}
+		}
+		hintUI.text = "";
 	}
 }
+
+//in-class code
+/*
+	void OnTriggerStay (Collider active){ // could use this
+		//can maybe parenting to pick up items
+		if (activator.tag == "Pickup"){
+			activator.transform.SetParent (transform);
+		}
+	}*/
--- week05-07/Assets/Scripts/PlayerControl.cs
+++ week05-07/Assets/Scripts/PlayerControl.cs
@@ -1,22 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour {
 
 	public float moveSpeed = 5f;
-	public Text hintUI;
-	public Text inventoryUI;
-	public Text controlsUI;
 	public Transform myCamera; //assign in Inspector
-
+	
 	Rigidbody rbody;
 	Vector3 inputVector; // remembers what direction I want to go in, based on input
-
-	bool canInteract = false; // determines if player can use nearItem
-	bool hasItem = false;
-	GameObject nearItem;
-
 	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody>(); // remember shortcut to access rigidbody
@@ -24,7 +15,6 @@ public class PlayerControl : MonoBehaviour {
 
 	// Update is called once per frame & for Input & Graphics
 	void Update () {
-		controlsUI.text = "[WASD] to move\nMouse to look\n[Space] to wake up";
 		//float jump = Input.GetButtonDown("Jump") ? 1f : 0f; //if getbutton down, set jump to 5, else set to 0
 		inputVector = new Vector3(Input.GetAxis("Horizontal"), 0 /* jump*/, Input.GetAxis ("Vertical"));
 		//Debug.Log ("input: " + inputVector);
@@ -32,20 +22,6 @@ public class PlayerControl : MonoBehaviour {
 		float mouseY = - Input.GetAxis("Mouse Y");
 		transform.Rotate(0f, mouseX, 0f);
 		myCamera.Rotate(mouseY, 0f, 0f);
-
-		if(Input.GetKeyDown (KeyCode.E)){
-			if (canInteract && !hasItem){
-				inventoryUI.text += nearItem.name + "\n";
-				hintUI.text = "Picked up " + nearItem.name;
-				hasItem = true;
-			}
-		}
-
-		//restart level
-		controlsUI.text += "\n[R] to restart";
-		if(Input.GetKeyDown(KeyCode.R)){
-			Application.LoadLevel(0);
-		}
 	}
 
 	// FixedUpdate is when Physics runs	
@@ -58,32 +34,7 @@ public class PlayerControl : MonoBehaviour {
 		//Debug.Log ("velocity: " + rbody.velocity);
 	}
 
-	void OnTriggerEnter (Collider activator) { // this parameter will get automatically get filled in with the thing that enters it
-		if (activator.gameObject.tag == "Item"){
-			hintUI.text = "[E] to pick up";	
-			nearItem = activator.gameObject;
-			canInteract = true;
-		} 
-		if (activator.gameObject.tag == "Hazard"){
-			hintUI.text = "you are dying right now";
-		}
-	}
-	void OnTriggerExit (Collider deactivated) {
-		if (deactivated.gameObject.tag == "Item"){
-			canInteract = false;
-			if (hasItem){
-				deactivated.gameObject.SetActive (false);
-			}
-		}
-		hintUI.text = "";
-	}
+	void OnTriggerEnter(Collider activator){
 
-	//in-class code
-	/*
-	void OnTriggerStay (Collider active){ // could use this
-		//can maybe parenting to pick up items
-		if (activator.tag == "Pickup"){
-			activator.transform.SetParent (transform);
-		}
-	}*/
+	}
 }

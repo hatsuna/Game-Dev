d11cf3caa2f9be0440be01926e7475c763e33295
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2015-10-27 16:46:35 -0400

    Week09 classwork

---
+++ week09/Assets/Scripts/BallRobot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRobot : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// move the ball forward by 5 units per second
+		transform.position += transform.forward * Time.deltaTime * 5f;
+
+		// if you sense a wall in front of you, turn right
+		Ray ray = new Ray( transform.position, transform.forward);
+		Debug.DrawRay (ray.origin, 
+		// now shoot the raycast
+		if (Physics.Raycast(ray, 2f)){
+			// turn right
+			float rand = Random.value;
+			if (rand >= 0.5f){
+				transform.Rotate(0, 90, 0);
+			}else{
+				transform.Rotate(0, -90, 0);
+			}
+		}
+	}
+}
---
+++ week09/Assets/Scripts/RaycastDemo.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class RaycastDemo : MonoBehaviour {
+
+	public Transform cube; // assign in Inspector
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(1)){
+			// generate a ray based on the mouse cursor position
+			Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition);
+			// generate a "RaycastHit" to remember where the raycast hit
+			RaycastHit rayHit = new RaycastHit();
+
+			if (Physics.Raycast (ray, out rayHit, 1000f )){
+				Debug.Log( "you are putting your cursor over a collider");
+				Debug.DrawRay ( ray.origin, ray.direction * rayHit.distance, Color.cyan);
+				cube.position = rayHit.point; // move cube to where it hit
+				cube.up = rayHit.normal; // aligns the cube with the sphere
+			}
+		}
+	}
+}

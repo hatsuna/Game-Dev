f75bc79cbeea07663afc960d7dbda1dfe79d23dd
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2015-11-05 14:53:43 -0500

    Week10 classwork update

---
+++ week10/Assets/Scripts/FishGod.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // to use lists
+
+public class FishGod : MonoBehaviour {
+
+	public Fish fishPrefab; // the fish prefab we clone
+	List<Fish> allTheFish = new List<Fish>(); // list of all the fish clones
+
+	// Use this for initialization
+	void Start () {
+		int fishCount = 0; // Remember how many fish we have made
+		while (fishCount < 100){
+			Fish newFishClone = (Fish)Instantiate ( fishPrefab, Random.insideUnitSphere * 100f, Random.rotation);
+			newFishClone.transform.localScale *= Random.Range (1.0f, 5.0f);
+			newFishClone.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1);
+			allTheFish.Add(newFishClone);
+			fishCount ++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){ // is space is pressed, fish converge on (25,13,17)
+			foreach(Fish fish in allTheFish){
+				fish.SetDestination(new Vector3(25f, 13f, 17f));
+			}
+		}
+
+		// mouse cursor raycast
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit rayHit = new RaycastHit();
+		if(Physics.Raycast( ray, out rayHit, 100f)){
+			Debug.DrawRay(ray.origin, ray.direction * rayHit.distance, Color.cyan);
+			foreach(Fish fish in allTheFish){
+				fish.SetDestination(rayHit.point);
+			}
+		}
+
+	}
+}
---
+++ week10/Assets/Scripts/ForestGen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //needed for Lists
+
+public class ForestGen : MonoBehaviour {
+
+	public GameObject treePrefab; //assign in inspector
+	List<GameObject> listOfTrees = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		/*int treeCounter = 0; // how many trees have been cloned
+		while (treeCounter < 49){ // keep repeating all the code until condition is false
+			GameObject newTree = (GameObject)Instantiate ( treePrefab, new Vector3( Random.Range(-10f, 10f), 1f, Random.Range (-10f, 10f)), Quaternion.identity); 
+			listOfTrees.Add(newTree);
+			treeCounter++;
+		}*/
+		while (listOfTrees.Count < 49){ // keep repeating all the code until condition is false
+			GameObject newTree = (GameObject)Instantiate ( treePrefab, new Vector3( Random.Range(-10f, 10f), 1f, Random.Range (-10f, 10f)), Quaternion.identity); 
+			listOfTrees.Add(newTree);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if I press Up, grow all the trees
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			for ( int x = 0; x < listOfTrees.Count; x++){
+				listOfTrees[x].transform.localScale *= 1.1f;
+			}
+		}
+	}
+}

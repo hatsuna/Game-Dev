72fa296c330523dbb7f5019b7e3f9fa4d5d15b50
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2015-11-30 15:26:06 -0500

    adding week11

---
+++ week11/Assets/SphereInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereInput : MonoBehaviour {
+
+	Animator myAnimator; //assign using getComponent;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>(); // cache shortcut
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.Space)){
+			myAnimator.SetBool("IsMoving", true);
+			transform.position += transform.forward * 0.5f; // move forward
+		}
+		else{
+			myAnimator.SetBool("IsMoving", false);
+		}
+	}
+}

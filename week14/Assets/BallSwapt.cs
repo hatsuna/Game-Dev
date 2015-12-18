8766e24ebada514f13ac999baa665cc5516c1633
[Hatsuna] <ixnoblesseobligexi@gmail.com>
2015-12-18 14:50:18 -0500

    week14

---
+++ week14/Assets/BallSwapt.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSwapt : MonoBehaviour {
+
+	public Transform ball1,ball2;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(BallSwaptCoroutine());
+	}
+
+
+	IEnumerator BallSwaptCoroutine(){
+		while(true){
+			Vector3 ball1Start = ball1.position;
+			Vector3 ball2Start = ball2.position;
+
+			bool playSound = false;
+
+			float t = 0f; // counter variable, "t" = "time"
+			while (t<1f){
+				t += Time.deltaTime; // make t bigger
+				ball1.position = Vector3.Lerp (ball1Start, ball2Start, t);
+				ball2.position = Vector3.Lerp (ball2Start, ball1Start, t);
+				if (t > 0.45f && t < 0.55f && playSound == false){
+					GetComponent<AudioSource>().Play (); // play sound
+					playSound = true;
+				}
+				yield return 0;
+			}
+		}
+	}
+}
---
+++ week14/Assets/CoroutineDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(MyFirstCoroutine());
+	}
+
+	IEnumerator MyFirstCoroutine(){
+		Debug.Log ("coroutine started");
+		//a yield tells Unity to pause in this function
+		yield return 0; // wait 1 frame
+		Debug.Log ("ok, I waited for 1 frame, now I'm continuing!...");
+		yield return null; // same as zero
+		yield return 0;
+		Debug.Log ("Ok I waited 2 more frames");
+		yield return new WaitForSeconds(1f); // wait for 1 second
+		Debug.Log ("I'm done waiting");
+	}
+}
